0b0f87c449b1663a14fea2383bdde7f1b2ce5cae
julkar nain <julkarnaincse@gmail.com>
2020-06-11 02:02:49 +0600

    Setup project

---
+++ app/src/main/java/com/julkar/nain/currencyconverter/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.julkar.nain.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.julkar.nain.currencyconverter.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
