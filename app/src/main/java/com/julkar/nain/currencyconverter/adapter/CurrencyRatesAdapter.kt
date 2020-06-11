ab2df41bace8024ec09ef6c5740a18747847323b
julkar nain <julkarnaincse@gmail.com>
2020-06-12 05:27:10 +0600

    Add adapter for showing rates

---
+++ app/src/main/java/com/julkar/nain/currencyconverter/adapter/CurrencyRatesAdapter.kt
@@ -0,0 +1,41 @@
+package com.julkar.nain.currencyconverter.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.julkar.nain.currencyconverter.R
+import kotlinx.android.synthetic.main.main_activity.view.*
+
+/**
+ * @author Julkar Nain
+ * @since 12 Jun 2020
+ */
+
+class CurrencyRatesAdapter : RecyclerView.Adapter<CurrencyRatesAdapter.ViewHolder>(){
+
+    private var list = listOf<String>()
+
+    fun updateList(list: List<String>){
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(android.R.layout.simple_list_item_1, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.count()
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.text.setText(list[position])
+    }
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val text = view.findViewById<TextView>(android.R.id.text1)
+    }
+}
